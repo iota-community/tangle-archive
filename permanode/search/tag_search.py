9f6b8bf0debb7b7f1062de49724b3e173c2b52b7
broganjb <jamesfbrogan@gmail.com>
2018-01-12 22:43:40 +0000

    Added tag search functionality for Cassandra

--- permanode/search/tag_search.py
+++ permanode/search/tag_search.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from iota import Address, Bundle, Transaction, TryteString, Tag
 from permanode.models import AddressModel, TransactionModel, BundleHashModel, TagModel, TransactionHashModel
 from permanode.search import search
@@ -11,13 +10,26 @@ def tag_search(search_string):
     api = IotaApi()
 
     tags, tags_status_code = api.find_transactions(tags=[search_string])
-    print(tags, file=sys.stderr)
+
+    '''
+        if the full node does not contain transactions
+        with this tag, check Cassandra
+    '''
 
     if not tags['hashes']:
+        tag_ref = TagModel.objects.filter(tag=search_string)
+        tag_obj = [res.as_json() for res in tag_ref]
+
+        if not tag_obj:
+            return {
+
+            }
+
+        txs = TransactionModel.objects.filter(id__in=[t['id'] for t in tag_obj])
+
         return {
-
+            'transactions': [res.as_json() for res in txs]
         }
-
 
     else:
         if tags_status_code == 503 or tags_status_code == 400:
@@ -26,7 +38,6 @@ def tag_search(search_string):
         elif tags_status_code == 200:
             transaction_hashes, transaction_hashes_status_code = api.get_trytes(tags['hashes'])
             all_transaction_hashes = []
-            print(transaction_hashes, file=sys.stderr)
 
             for tryte in transaction_hashes['trytes']:
                 all_transaction_hashes.append(tryte[2430:2511])
